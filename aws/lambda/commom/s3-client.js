599c1e83b0e3ad20b127f7a6b34d55b098051d42
Ailton Loures Ferreira <ailton.ferreira@stoneage.com.br>
2021-12-11 16:22:30 -0300

    add webpack

--- aws/lambda/commom/s3-client.js
+++ aws/lambda/commom/s3-client.js
@@ -3,7 +3,37 @@
 const { S3 } = require('aws-sdk');
 
 const S3Client = {
+    // Props
+    bucket: null,
+    client: new S3(),
+    // Methods
+    setBucket(bucket) {
+        this.bucket = bucket;
+    },
+    async get({ fileName }) {
+        const object = await this.client.getObject({
+            Bucket: this.bucket,
+            Key: fileName
+        }).promise();
 
+        if (!object)
+            throw new Error(`S3Client :: This file not found ${fileName}`);
+
+        return object;
+    },
+    async put({ fileName, data, contentType = 'application/pdf' }) {
+        const object = await this.client.putObject({
+            Bucket: this.bucket,
+            Key: fileName,
+            Body: data,
+            ContentType: contentType
+        }).promise();
+
+        if (!object)
+            throw new Error(`S3Client :: There was an error writing this file ${fileName}`);
+
+        return object;
+    }
 };
 
 module.exports = S3Client;
--- aws/lambda/commom/sqs-client.js
+++ aws/lambda/commom/sqs-client.js
@@ -3,7 +3,25 @@
 const { SQS } = require('aws-sdk');
 
 const SQSClient = {
+    // Props
+    region: null,
+    client: new SQS({
+        apiVersion: 'latest',
+        region: this.region
+    }),
+    // Methods
+    setRegion(region) {
+        this.region = region;
+    },
+    async addQueue({ queueUrl, data }) {
+        const queue = await this.client.sendMessage({
+            QueueUrl: queueUrl,
+            MessageBody: data
+        }).promise();
 
+        if (!queue)
+            throw new Error(`SQSClient :: There was an error creating this queue ${JSON.stringify(data)}`);
+    }
 };
 
 module.exports = SQSClient;
